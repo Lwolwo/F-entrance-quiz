1dfb3296b1afdcb356966ca179cc242bf90d5925
Lwolwo <26643253+Lwolwo@users.noreply.github.com>
2020-10-28 00:18:46 +0800

    feat: 展示分组表格

--- src/App/components/header/Header.js
+++ src/App/components/header/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sortMemberList: []
+      sortMemberList: [],
     };
   }
 
@@ -24,11 +24,31 @@ class Header extends Component {
   }
 
   render() {
+    const { sortMemberList } = this.state;
     return (
-      <div className="header">
-        <h1>分组列表</h1>
-        <div className="button" onClick={() => this.sortMember()}>
-          分组学员
+      <div className="container">
+        <div className="header">
+          <h1>分组列表</h1>
+          <div className="button" onClick={() => this.sortMember()}>
+            分组学员
+          </div>
+        </div>
+        <div className="group-table">
+          {sortMemberList.length !== 0 &&
+            sortMemberList.map((item, index) => {
+              return (
+                <div className="group">
+                  <div className="title">第{index + 1}组</div>
+                  <div className="group-item">
+                    {
+                      item.map((itemChild) => {
+                        return <div className="member-card">{itemChild.id}. {itemChild.name}</div>
+                      })
+                    }
+                  </div>
+                </div>
+              );
+            })}
         </div>
       </div>
     );
